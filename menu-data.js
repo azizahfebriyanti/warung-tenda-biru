3c8bf4d41aa4c2ce50a37ccfc44dbf4c8b27c8c0
azizahfebriyanti <azizahfebriyanti20@gmail.com>
2025-06-08 15:33:37 +0700

    update menu

---
+++ menu-data.js
@@ -0,0 +1,26 @@
+export const menuItems = [
+  {
+    title: "Ayam Bakar Bumbu Rujak",
+    desc: "Grilled chicken in spicy-sweet rujak sauce.",
+    price: 20000,
+    available: true,
+    img: "img/ayambakarbumburujak.jpg",
+    link: "dish-ayam.html"
+  },
+  {
+    title: "Woku",
+    desc: "Spicy Manadonese-style chicken with herbs.",
+    price: 18000,
+    available: true,
+    img: "img/woku.jpg",
+    link: "dish-woku.html"
+  },
+  {
+    title: "Pesmol",
+    desc: "Fried fish in turmeric coconut sauce.",
+    price: 17000,
+    available: false,
+    img: "img/pesmol.jpg",
+    link: "dish-pesmol.html"
+  }
+];
