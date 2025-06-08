ba04881b906de770811419b600738c0e51250899
azizahfebriyanti <azizahfebriyanti20@gmail.com>
2025-06-08 16:03:12 +0700

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -2,25 +2,22 @@ export const menuItems = [
   {
     title: "Ayam Bakar Bumbu Rujak",
     desc: "Grilled chicken in spicy-sweet rujak sauce.",
-    price: Rp15.000,
+    price: "Rp15.000",
     available: true,
     img: "ayambakarbumburujak.jpg",
-    link: "dish-ayam.html"
   },
   {
     title: "Woku",
     desc: "Spicy Manadonese-style chicken with herbs.",
-    price: 18000,
+    price: "Rp8.000",
     available: true,
     img: "woku.jpg",
-    link: "dish-woku.html"
   },
   {
     title: "Pesmol",
     desc: "Fried fish in turmeric coconut sauce.",
-    price: 17000,
+    price: "Rp13.000",
     available: false,
     img: "pesmol.jpg",
-    link: "dish-pesmol.html"
   }
 ];
